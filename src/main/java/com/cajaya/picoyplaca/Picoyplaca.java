628004e09cd901cefcf2d26c5cc246e94c482e3c
Carlos Jaya <cajaya1@espe.edu.ec>
2026-06-20 21:06:34 -0500

    chore: initial setup for the project, MVC structure and domain packages. V0.01

---
+++ src/main/java/com/cajaya/picoyplaca/Picoyplaca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cajaya.picoyplaca;
+
+/**
+ *
+ * @author cajh1
+ */
+public class Picoyplaca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
